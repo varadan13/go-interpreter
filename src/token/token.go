34ecf1349dce27eeab6dc30524cc157b62418da9
varadan13 <rcaesar1996@gmail.com>
2022-09-18 10:44:50 +0530

    define basic tokens

--- src/token/token.go
+++ src/token/token.go
@@ -6,3 +6,29 @@ type Token struct {
 	Type TokenType
 	Literal string
 }
+
+const  {
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+}
